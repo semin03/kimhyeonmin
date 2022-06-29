d7c7da80898d50b66c0b6825fd40c8d38537dc90
semin03 <103947428+semin03@users.noreply.github.com>
2022-06-29 14:20:50 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+student = [] ;
+n = int(input("사람 수 입력 : ")) ;
+for i in range(n) : 
+    name = input("이름 입력 : ") ;
+    math = int(input("수학 점수 : ")) ;
+    eng = int(input("영어 점수 : ")) ;
+    total = math + eng ;
+    avg = (total / 2) ;
+    app = {
+                "name": name,
+                "math": math,
+                "eng": eng,
+                "total": total,
+                "avg": avg,
+                
+            }
+    student.append(app) ; 
+print(len[student]) ;
+print(student) ;
